fd9997cbbfe3bac9670a01b2386511aec9e10338
joao96 <jvpoletti@gmail.com>
2021-08-30 15:47:38 -0300

    Dashboard responsivo

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -72,7 +72,7 @@ export default function Dashboard() {
 
         <SimpleGrid flex="1" gap="4" minChildWidth="320px" align="flex-start">
           <Box
-            p="8"
+            p={["6", "8"]} //responsividade
             bg="gray.800"
             borderRadius={8}
             // pb="4"
@@ -82,7 +82,7 @@ export default function Dashboard() {
             </Text>
             <Chart options={options} series={series} type="area" height={160} />
           </Box>
-          <Box p="8" bg="gray.800" borderRadius={8} pb="4">
+          <Box p={["6", "8"]} bg="gray.800" borderRadius={8} pb="4">
             <Text fontSize="lg" mb="4">
               Taxa de abertura
             </Text>
