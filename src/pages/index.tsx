588c621f56c799ab92824b2ea95b72a087d4d446
joao96 <jvpoletti@gmail.com>
2021-08-31 12:15:46 -0300

    Validação dos dados

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,23 @@
 import { Flex, Button, Stack } from "@chakra-ui/react";
 import { Input } from "../components/Form/Input";
 import { useForm, SubmitHandler } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 type SignInFormData = {
   email: string;
   password: string;
 };
 
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required("E-mail obrigatório").email("E-mail inválido"),
+  password: yup.string().required("Senha obrigatória"),
+});
+
 export default function SignIn() {
-  const { register, handleSubmit, formState } = useForm();
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
 
   const { errors } = formState;
 
@@ -35,12 +44,14 @@ export default function SignIn() {
             name="email"
             type="email"
             label="E-mail"
+            error={errors.email}
             {...register("email")}
           />
           <Input
             name="password"
             type="password"
             label="Senha"
+            error={errors.password}
             {...register("password")}
           />
         </Stack>
